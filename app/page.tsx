a5a34fa8344c85ae5d32246fc42769d32590ca99
Shreyask24 <shreyask495@gmail.com>
2025-06-13 23:45:25 +0530

    implement session history

--- app/page.tsx
+++ app/page.tsx
@@ -3,37 +3,27 @@ import CompanionsList from '@/components/CompanionsList'
 import CTA from '@/components/CTA'
 import { Button } from '@/components/ui/button'
 import { recentSessions } from '@/constants'
+import { getAllCompanions, getRecentSessions } from '@/lib/actions/actions'
+import { getSubjectColor } from '@/lib/utils'
 import React from 'react'
 
-const Page = () => {
+const Page = async () => {
+  const companions = await getAllCompanions({ limit: 3 })
+  const recentSessionCompanies = await getRecentSessions(10);
+
   return (
     <main>
       <h1 className='text-2xl underline'>Popular Companions</h1>
 
       <section className='home-section'>
-        <CompanionCard
-          id="124"
-          name="Neura the Brainy Explorer"
-          topic="Neural Network of the Brain"
-          subject="science"
-          duration={45}
-          color="#ffda6e" />
+        {companions.map((companion) => (
+          <CompanionCard
+            key={companion.id}
+            {...companion}
+            color={getSubjectColor(companion.subject)}
+          />
+        ))}
 
-        <CompanionCard
-          id="456"
-          name="Countsy the Number Wizard"
-          topic="Derivatives"
-          subject="maths"
-          duration={30}
-          color="#e5d0ff" />
-
-        <CompanionCard
-          id="789"
-          name="Verba the Vocubulary Builder"
-          topic="English Literature"
-          subject="Language"
-          duration={30}
-          color="#bde7ff" />
       </section>
 
       <section className='home-section'>
